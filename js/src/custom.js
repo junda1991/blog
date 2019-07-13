ea7fab3d258bc1f51f562a1d58cf3bcda11986ea
zhujunda1991 <327812345@qq.com>
2019-07-13 09:10:01 +0800

    Site updated: 2019-07-13 09:10:00

--- js/src/custom.js
+++ js/src/custom.js
@@ -1,4 +1,1 @@
-/* 返回随机颜色 */
-function randomColor() {
-    return "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")";
-}
+function randomColor(){return"rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"}
